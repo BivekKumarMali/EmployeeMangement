e2800c063161844d018ff1698f48597e20aeba54
BivekKumarMali <bivekkumar@promactinfo.com>
2020-09-24 19:11:50 +0530

    Disable and Enable form button in profile page

--- EmployeeManagement.App/src/app/home/profile/profile.component.ts
+++ EmployeeManagement.App/src/app/home/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/models/employee';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+  Employee: Employee;
+  Edit = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  EnableEdit() {
+    this.Edit = this.Edit ? false : true;
+  }
 }
